46f8213df6faf0dc32c989a6467114cfc0e7ba41
rzwerner <36875257+rzwerner@users.noreply.github.com>
2025-06-19 09:55:39 -0500

    Update vite.config.js

--- unsettled-agitators-pwa/vite.config.js
+++ unsettled-agitators-pwa/vite.config.js
@@ -1,5 +1,10 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+export default defineConfig({
   plugins: [react()],
-};
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
